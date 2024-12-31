093d7380a71fb2bdcdfbce99e237e4d4634023a6
YoonDongSik <ehdtlr9731@naver.com>
2024-12-31 17:39:46 +0900

    미로 과제 추가

--- Maze/Maze/main.cpp
+++ Maze/Maze/main.cpp
@@ -1,6 +1,16 @@
 #include "Maze.h" 
 #include <iostream> 
 
+// 미로생성알고리즘 사용
+
+// 무조건 길 뚫기
+// 시작점-도착점까지 랜덤 경로 만들기 (벽을 일정 길이 이상)
+
+// 추가 시스템 생각해보기
+// 특수문자로 맵 변경
+// 타임어택
+// 타이틀
+
 #ifdef _WIN32 
 #include <windows.h> 
 #else 
